ae8006db222676f7d05b165786508a87cab5d807
Ultoman <jabonasera@gmail.com>
2025-06-23 09:00:15 -0400

    update unit stats

--- src/data/resources/unit_resource.gd
+++ src/data/resources/unit_resource.gd
@@ -3,12 +3,12 @@ class_name UnitResource
 
 @export var name: String = "Unit"
 @export var animation_resource: SpriteFrames
-@export var max_health: int = 100
+@export var max_health: int = 10
 @export var health: int = max_health
 @export var max_movement: int = 5
-@export var movement: int = 5
-@export var attack: int = 20
-@export var defense: int = 10
+@export var movement: int = max_movement
+@export var attack: int = 2
+@export var defense: int = 1
 @export var attack_range: int = 1
 
 
